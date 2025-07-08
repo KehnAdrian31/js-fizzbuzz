982b929b46c08dc34b544f49507a6472992437f0
KehnAdrian31 <kehn000panganibangmail.com>
2025-07-08 16:46:37 +0200

    [add] buzz multiplo di 5

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 //ciclo i numeri da 1 a 100
 for(let i = 1; i <=100; i++){
+
+  //verifico se il numero è multiplo di 3
   if (i % 3 === 0){
     console.log(`Fizz`)
+  }
+
+  //verifico se il numero è multiplo di 5
+  else if(i % 5 === 0){
+    console.log(`Buzz`)
   }
 }
