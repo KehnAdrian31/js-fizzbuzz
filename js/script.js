df7ad23be49c2f7fc49131ec17b625914fac049e
KehnAdrian31 <kehn000panganibangmail.com>
2025-07-08 16:58:34 +0200

    [add] multiplo di 3 e 5 [fix] ordine

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
 //ciclo i numeri da 1 a 100
 for(let i = 1; i <=100; i++){
 
-  //verifico se il numero è multiplo di 3
-  if (i % 3 === 0){
-    console.log(`Fizz`)
+ // verifico se il numero è multiplo di 3 e 5
+  if(i % 3 === 0 && i % 5 === 0){
+    console.log(`FizzBuzz`)
   }
 
-  //verifico se il numero è multiplo di 5
+   //verifico se il numero è multiplo di 5
   else if(i % 5 === 0){
     console.log(`Buzz`)
+  }
+
+  //verifico se il numero è multiplo di 3
+   else if (i % 3 === 0){
+    console.log(`Fizz`)
+  }
+  
+  else{
+    console.log(i)
   }
 }
